3293a605cc5a42cf9c973a000a085397e022158e
Alexandr <slicarenko@gmail.com>
2026-03-18 14:06:33 +0300

    Start project Kalita

---
+++ src/main/java/ru/slisarenko/kalita/KalitaApplication.java
@@ -0,0 +1,13 @@
+package ru.slisarenko.kalita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KalitaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/slisarenko/kalita/KalitaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.slisarenko.kalita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalitaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
